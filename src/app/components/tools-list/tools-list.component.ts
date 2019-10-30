3fe29a50e3e21b76656b75a8a17b71fcf694b4a7
rfdsantos25 <rafael.santos@digifred.com.br>
2019-10-29 23:40:53 -0300

    Implemented Search and Delete

--- src/app/components/tools-list/tools-list.component.ts
+++ src/app/components/tools-list/tools-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ToolsService} from '../../services/tools.service';
 import {Tool} from '../../models/tool';
+import {HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-tools-list',
@@ -9,17 +10,50 @@ import {Tool} from '../../models/tool';
 })
 export class ToolsListComponent implements OnInit {
 
+  tagsOnly = false;
   tools: Array<Tool> = [];
 
   constructor(private toolsService: ToolsService) { }
 
   ngOnInit() {
-    this.toolsService.find().subscribe( (tools: Array<Tool>) => {
-      this.tools = tools;
-    }, (error => {
+    this.findAll();
+  }
+
+  private findAll() {
+    this.toolsService.find().subscribe((tools: Array<Tool>) => {
+        this.tools = tools;
+      }, (error => {
         alert(error);
       })
     );
   }
+
+  search(event: any) {
+    if (event.target.value) {
+      let httpParams;
+      if (this.tagsOnly) {
+        httpParams = new HttpParams().set('tags_like', event.target.value);
+      } else {
+        httpParams = new HttpParams().set('q', event.target.value);
+      }
+      this.toolsService.find(httpParams).subscribe( (tools: Array<Tool>) => {
+          this.tools = tools;
+        }, (error => {
+          alert(error);
+        })
+      );
+    } else {
+      this.findAll();
+    }
+ }
+
+ delete(element: Tool) {
+   this.toolsService.delete(element.id).subscribe(() => {
+       this.tools.splice(this.tools.findIndex( (tool => tool.id === element.id)), 1);
+     }, (error => {
+       alert(error);
+     })
+   );
+ }
 
 }
